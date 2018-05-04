fd025faf87813f06813bedfb2c02c4d32c6c5ab4
ChinaSea <hpuzhuhai@163.com>
2018-05-04 17:41:36 +0800

    完成页面样式布局

--- src/components/page/pos.vue
+++ src/components/page/pos.vue
@@ -1,16 +1,294 @@
 <template>
 	<div class="pos">
-		
+		<el-row>
+			<el-col :span="8" id="order-list" class="post-order">
+				<div class="menu">
+					<el-tabs>
+						<el-tab-pane label="点餐">
+							<el-table :data="tableData" border style="width:100%">
+								<el-table-column prop="goodsName" label="商品名称"></el-table-column>
+								<el-table-column prop="count" label="数量"></el-table-column>
+								<el-table-column prop="price" label="金额"></el-table-column>
+								<el-table-column label="操作" width="100" fixed="right">
+									<template slot-scope="scope">
+										<el-button type="text" size="small">增加</el-button>
+										<el-button type="text" size="small">删除</el-button>
+									</template>
+								</el-table-column>
+							</el-table>
+							<div class="totalDiv">
+								<small>数量：</small>
+								<strong>{{totalCount}}</strong>&nbsp;&nbsp;&nbsp;&nbsp;
+								<small>总计：</small>
+								￥<strong>{{totalPrice}}</strong>元
+							</div>
+							<div class="order-btn">
+								<el-button type="warning">挂单</el-button>
+								<el-button type="danger">删除</el-button>
+								<el-button type="success">结账</el-button>
+							</div>
+						</el-tab-pane>
+						<el-tab-pane label="挂单">挂单</el-tab-pane>
+						<el-tab-pane label="外卖">外卖</el-tab-pane>
+					</el-tabs>
+				</div>
+				
+			</el-col>
+			<el-col :span="16">
+				<div class="title">常用商品</div>
+				<div class="often-goods-list">
+					<ul>
+						<li v-for="goods in oftenGoods">
+							<span>{{goods.goodsName}}</span>
+							<span class="goodsPrice">￥{{goods.price}}元</span>
+						</li>
+						
+					</ul>
+				</div>
+				<div class="goods-type">
+					<el-tabs>
+						<el-tab-pane label="汉堡">
+							<ul class='cookList'>
+							    <li v-for="goods in type0Goods">
+							        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+							        <span class="foodName">{{goods.goodsName}}</span>
+							        <span class="foodPrice">￥{{goods.price}}元</span>
+							    </li>
+
+							</ul>
+						</el-tab-pane>
+						<el-tab-pane label="小食"></el-tab-pane>
+						<el-tab-pane label="饮料"></el-tab-pane>
+						<el-tab-pane label="套餐"></el-tab-pane>
+					</el-tabs>
+				</div>
+			</el-col>
+		</el-row>
 	</div>
 </template>
 
 <script>
+	let tableData = [{
+          
+		          goodsName: '可口可乐',
+		          price: 8,
+		          count:1
+		        }, {
+		          
+		          goodsName: '香辣鸡腿堡',
+		          price: 15,
+		          count:1
+		        }, {
+		         
+		          goodsName: '爱心薯条',
+		          price: 8,
+		          count:1
+		        }, {
+		         
+		          goodsName: '甜筒',
+		          price: 8,
+		          count:1
+		        }];
+	let oftenGoods = [
+          {
+              goodsId:1,
+              goodsName:'香辣鸡腿堡',
+              price:18
+          }, {
+              goodsId:2,
+              goodsName:'田园鸡腿堡',
+              price:15
+          }, {
+              goodsId:3,
+              goodsName:'和风汉堡',
+              price:15
+          }, {
+              goodsId:4,
+              goodsName:'快乐全家桶',
+              price:80
+          }, {
+              goodsId:5,
+              goodsName:'脆皮炸鸡腿',
+              price:10
+          }, {
+              goodsId:6,
+              goodsName:'魔法鸡块',
+              price:20
+          }, {
+              goodsId:7,
+              goodsName:'可乐大杯',
+              price:10
+          }, {
+              goodsId:8,
+              goodsName:'雪顶咖啡',
+              price:18
+          }, {
+              goodsId:9,
+              goodsName:'大块鸡米花',
+              price:15
+          }, {
+              goodsId:20,
+              goodsName:'香脆鸡柳',
+              price:17
+          }
+          
+      ];
+      let type0Goods = [
+          {
+              goodsId:1,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+              goodsName:'香辣鸡腿堡',
+              price:18
+          }, {
+              goodsId:2,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'田园鸡腿堡',
+              price:15
+          }, {
+              goodsId:3,
+              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+              goodsName:'和风汉堡',
+              price:15
+          }, {
+              goodsId:4,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'快乐全家桶',
+              price:80
+          }, {
+              goodsId:5,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'脆皮炸鸡腿',
+              price:10
+          }, {
+              goodsId:6,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+              goodsName:'魔法鸡块',
+              price:20
+          }, {
+              goodsId:7,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+              goodsName:'可乐大杯',
+              price:10
+          }, {
+              goodsId:8,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+              goodsName:'雪顶咖啡',
+              price:18
+          }, {
+              goodsId:9,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'大块鸡米花',
+              price:15
+          }, {
+              goodsId:20,
+               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+              goodsName:'香脆鸡柳',
+              price:17
+          }
+          
+      ];
 	export default{
 		name: 'pos',
 		data() {
 			return {
-				msg: '哈啊哈哈哈哈'
+				tableData: tableData,
+		        oftenGoods: oftenGoods,
+		        type0Goods: type0Goods,
+		        totalCount: 0,
+		        totalPrice: 0
+
+
 			}
+		},
+		mounted: function() {
+			let clientHeight = document.body.clientHeight;
+			document.getElementById("order-list").style.height = clientHeight + "px";
+
 		}
+
 	}
 </script>
+<style>
+	.post{
+		font-size: 16px;
+	}
+	.post-order{
+		background-color: #F9FAFC;
+    	border-right: 1px solid #C0CCDA;
+	}
+	.menu{
+		padding: 0 5px 0 10px;
+	}
+	.totalDiv{
+		text-align: right;
+		font-size: 16px;
+		background-color: #fff;
+		border-bottom: 1px solid #E5E9F2;
+		padding: 10px;
+	}
+	.order-btn{
+		margin-top: 10px;
+		text-align: center;
+	}
+	.title{
+		height: 20px;
+		border-bottom: 1px solid #D3DCE6;
+		background-color: #F9FAFC;
+		padding: 10px 10px 9px 10px;
+	}
+	.often-goods-list {
+	    border-bottom: 1px solid #C0CCDA;
+	    height: auto;
+	    overflow: hidden;
+	    padding-bottom: 10px;
+	    background-color: #F9FAFC;
+	}
+	.often-goods-list ul li{
+		list-style: none;
+		float: left;
+		border: 1px solid #E5E9F2;
+		background-color: #fff;
+		padding: 10px;
+		margin: 5px;
+		cursor: pointer;
+	}
+	.goodsPrice{
+		color: #58B7FF;
+	}
+	.goods-type{
+		padding-left: 10px;
+		clear: both;
+	}
+	.cookList{
+		padding-left: 20px
+	}
+	.cookList li{
+		list-style: none;
+		width: 23%;
+		border: 1px solid #E5E9F2;
+		height: auto;
+		float: left;
+		overflow: hidden;
+		padding: 2px;
+		margin: 2px;
+		background-color: #fff;
+		cursor: pointer;
+	}
+	.cookList li span{
+		display: block;
+		float: left;
+	}
+	.foodImg{
+		width: 40%;
+	}
+	.foodName {
+	    font-size: 18px;
+	    padding-left: 10px;
+	    color: brown;
+	}
+	.foodPrice {
+	    font-size: 16px;
+	    padding-left: 10px;
+	    padding-top: 10px;
+	}
+</style>
