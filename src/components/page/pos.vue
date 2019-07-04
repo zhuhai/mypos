aedfefdfe7bf0180081a345913d46b4203d3c931
zhuhai <hpuzhuhai@gmail.com>
2019-07-04 21:16:36 +0800

    修复异常

--- src/components/page/pos.vue
+++ src/components/page/pos.vue
@@ -38,7 +38,7 @@
 				<div class="title">常用商品</div>
 				<div class="often-goods-list">
 					<ul>
-						<li v-for="goods in oftenGoods" @click="addOrderList(goods)">
+						<li v-for="goods in oftenGoods" @click="addOrderList(goods)" :key="goods">
 							<span>{{goods.goodsName}}</span>
 							<span class="goodsPrice">￥{{goods.price}}元</span>
 						</li>
@@ -49,7 +49,7 @@
 					<el-tabs>
 						<el-tab-pane label="汉堡">
 							<ul class='cookList'>
-							    <li v-for="goods in type0Goods" @click="addOrderList(goods)">
+							    <li v-for="goods in type0Goods" @click="addOrderList(goods)" :key="goods">
 							        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 							        <span class="foodName">{{goods.goodsName}}</span>
 							        <span class="foodPrice">￥{{goods.price}}元</span>
@@ -59,7 +59,7 @@
 						</el-tab-pane>
 						<el-tab-pane label="小食">
 							<ul class='cookList'>
-							    <li v-for="goods in type1Goods" @click="addOrderList(goods)">
+							    <li v-for="goods in type1Goods" @click="addOrderList(goods)" :key="goods">
 							        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 							        <span class="foodName">{{goods.goodsName}}</span>
 							        <span class="foodPrice">￥{{goods.price}}元</span>
@@ -69,7 +69,7 @@
 						</el-tab-pane>
 						<el-tab-pane label="饮料">
 							<ul class='cookList'>
-							    <li v-for="goods in type2Goods" @click="addOrderList(goods)">
+							    <li v-for="goods in type2Goods" @click="addOrderList(goods)" :key="goods">
 							        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 							        <span class="foodName">{{goods.goodsName}}</span>
 							        <span class="foodPrice">￥{{goods.price}}元</span>
@@ -79,7 +79,7 @@
 						</el-tab-pane>
 						<el-tab-pane label="套餐">
 							<ul class='cookList'>
-							    <li v-for="goods in type3Goods" @click="addOrderList(goods)">
+							    <li v-for="goods in type3Goods" @click="addOrderList(goods)" :key="goods">
 							        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 							        <span class="foodName">{{goods.goodsName}}</span>
 							        <span class="foodPrice">￥{{goods.price}}元</span>
